a5e89a785c49d404f3e26365fa4e47aa56c26622
JayJuarez11 <Jay8Juarez@gmail.com>
2024-12-12 16:45:14 -0600

    removing print statements

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"math"
@@ -75,21 +74,17 @@ func determinePointsAwarded(receipt Receipt) int{
 			pointsAwarded++
 		}
 	}
-	fmt.Println("Points Awarded After Rule 1: ", pointsAwarded)
 	// Rule 2: 50 points if the total is a round dollar amount with no cents.
 	totalChange := receipt.Total[len(receipt.Total) - 2:]
 	if totalChange == "00" {
 		pointsAwarded += 50
 	}
-	fmt.Println("Points Awarded After Rule 2: ", pointsAwarded)
 	// Rule 3: 25 points if the total is a multiple of 0.25.
 	if totalChange == "00" || totalChange == "25" || totalChange == "50" || totalChange == "75" {
 		pointsAwarded += 25
 	}
-	fmt.Println("Points Awarded After Rule 3: ", pointsAwarded)
 	// Rule 4: 5 points for every two items on the receipt.
 	pointsAwarded = pointsAwarded + (len(receipt.Items) / 2) * 5
-	fmt.Println("Points Awarded After Rule 4: ", pointsAwarded)
 	// Rule 5: If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up
 	//to the nearest integer. The result is the number of points earned.
 	for _, item := range receipt.Items {
@@ -97,17 +92,14 @@ func determinePointsAwarded(receipt Receipt) int{
 		if len(trimmedShortDescription) % 3 == 0 {
 			itemPrice, _ := strconv.ParseFloat(item.Price, 64)
 			itemPointsAwarded:= int(math.Ceil(itemPrice * 0.2))
-			fmt.Println("Item Points Awarded: ", itemPointsAwarded)
 			pointsAwarded += itemPointsAwarded
 		}
 	}
-	fmt.Println("Points Awarded After Rule 5: ", pointsAwarded)
 	// Rule 6: 6 points if the day in the purchase date is odd.
 	purchaseDay := receipt.PurchaseDate[len(receipt.PurchaseDate) - 2:]
 	if purchaseDay[1] % 2 != 0 {
 		pointsAwarded += 6
 	}
-	fmt.Println("Points Awarded After Rule 6: ", pointsAwarded)
 	// Rule 7: 10 points if the time of purchase is after 2:00pm and before 4:00pm.
 	purchaseTime, _ := time.Parse("15:04", receipt.PurchaseTime)
 	afterTime, _ := time.Parse("15:04", "14:00")
@@ -115,7 +107,6 @@ func determinePointsAwarded(receipt Receipt) int{
 	if purchaseTime.After(afterTime) && purchaseTime.Before(beforeTime) {
 		pointsAwarded += 10
 	}
-	fmt.Println("Points Awarded After Rule 7: ", pointsAwarded)
 
 	return pointsAwarded
 }
